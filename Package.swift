ebd18088ee4dab5baebc92ca5ef556225cfb43a0
James <ptaclau@gmail.com>
2025-01-12 10:32:02 +0700

    Use binary

--- Package.swift
+++ Package.swift
@@ -1,23 +1,23 @@
-   // swift-tools-version:5.4
-   import PackageDescription
+// swift-tools-version:5.5
+import PackageDescription
 
-   let package = Package(
-       name: "ZaloSDK",
-       platforms: [
-           .iOS(.v9)
-       ],
-       products: [
-
-           .library(
-               name: "ZaloSDK",
-               targets: ["ZaloSDK"])
-       ],
-       targets: [
-           .target(
-               name: "ZaloSDK",
-               dependencies: [],
-               path: "ZaloSDK/Frameworks", // Adjust the path to your source files accordingly
-               resources: [] // Define any resources like images or storyboards if available
-           )
-       ],
-   )
+let package = Package(
+    name: "ZaloSDK-iOS",
+    platforms: [
+        .iOS(.v11)  // Minimum iOS version requirement
+    ],
+    products: [
+        .library(
+            name: "ZaloSDK",
+            targets: ["ZaloSDK"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "ZaloSDK",
+            url: "https://github.com/jigajames/ZaloSDK-iOS/releases/download/v1.0.0/ZaloSDK.xcframework.zip",
+            checksum: "23a5a1a9bdccad86acfe1402bd8769bc14d517997636e41b94aba2dd005de1f2"       // Replace with actual checksum
+        )
+    ]
+)
