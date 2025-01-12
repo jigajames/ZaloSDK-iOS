2b31d4dba7f166d7fe5a392d6319ab00a3c7d863
James <ptaclau@gmail.com>
2025-01-12 12:35:07 +0700

    Add ZaloSDKCoreKit

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "ZaloSDK",
-            targets: ["ZaloSDK"]
+            targets: ["ZaloSDK", "ZaloSDKCoreKit"]
         )
     ],
     dependencies: [],
@@ -17,7 +17,12 @@ let package = Package(
         .binaryTarget(
             name: "ZaloSDK",
             url: "https://github.com/jigajames/ZaloSDK-iOS/releases/download/v1.0.0/ZaloSDK.xcframework.zip",
-            checksum: "23a5a1a9bdccad86acfe1402bd8769bc14d517997636e41b94aba2dd005de1f2"       // Replace with actual checksum
+            checksum: "23a5a1a9bdccad86acfe1402bd8769bc14d517997636e41b94aba2dd005de1f2"
+        ),
+        .binaryTarget(
+            name: "ZaloSDKCoreKit",
+            url: "https://github.com/jigajames/ZaloSDK-iOS/releases/download/v1.0.0/ZaloSDKCoreKit.xcframework.zip",
+            checksum: "7074a23a0d3ef146797347e51d13d6f186188859ce2ebb8e3fff47fc05ece44f"
         )
     ]
 )
